dc596402508662a0778a19e46600073d1c62c21e
micheleferri01 <micheleferri095@gmail.com>
2025-12-15 13:48:52 +0100

    soluzione punto 2 strings

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,15 +1,21 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
-const name = "Michele"
+const name = "Michele";
 
-console.log(name)
+console.log(name);
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+const number = 2025;
+const text = "day one";
+const bool = true;
 
+console.log(number);
+console.log(text);
+console.log(bool);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
