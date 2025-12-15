21b8726cdc820e3c8c2f8418be150222b0c128c0
micheleferri01 <micheleferri095@gmail.com>
2025-12-15 13:42:13 +0100

    soluzione punto 1 strings

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,11 +1,15 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
+const name = "Michele"
+
+console.log(name)
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
